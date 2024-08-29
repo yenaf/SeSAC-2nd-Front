8268fcabf5ade36603ac828e1504b13371e0911a
DAIN302 <dbgnfk12@gmail.com>
2024-08-29 16:19:48 +0900

    add : page 생성

---
+++ frontend/src/pages/MyPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// 마이페이지
+export default function MyPage() {
+  return (
+    <div>MyPage</div>
+  )
+}
---
+++ frontend/src/pages/PostCreatePage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// 판매글 작성 페이지
+export default function PostCreatePage() {
+  return (
+    <div>PostCreatePage</div>
+  )
+}
