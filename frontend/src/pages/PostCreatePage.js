03a40012a1dc88195170938eaeba23b97b03e8e3
yenaf <spffhd0835@gmail.com>
2024-08-30 16:34:42 +0900

    Style : 게시물 작성 페이지 스타일작성

--- frontend/src/pages/PostCreatePage.js
+++ frontend/src/pages/PostCreatePage.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from "react";
+import "../styles/pages/PostCreatePage.scss";
 
 // 판매글 작성 페이지
 export default function PostCreatePage() {
   return (
-    <div>PostCreatePage</div>
-  )
+    <section>
+      <div class="inner">
+        <h2>판매글 작성</h2>
+        <form>
+          <div className="product-info">
+            <h3>상품명</h3>
+            <span>
+              <input type="text" />
+            </span>
+          </div>
+
+          <div className="product-info">
+            <h3>카테고리</h3>
+            <label htmlFor="kpop">
+              K-POP
+              <input type="radio" id="kpop" />
+            </label>
+          </div>
+
+          <div className="product-info">
+            <h3>상품유형</h3>
+            <input type="text" />
+          </div>
+          <div className="product-info">
+            <h3>상품상태</h3>
+            <input type="text" />
+          </div>
+
+          <div className="product-info">
+            <h3>상품가격</h3>
+            <input type="text" />
+          </div>
+
+          <div className="product-info">
+            <h3>상품정보</h3>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
