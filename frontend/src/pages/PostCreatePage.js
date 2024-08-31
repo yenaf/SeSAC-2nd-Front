194c44e8f24f80fea5acce25e0b6aa819dc67b1a
yena <spffhd0835@gmail.com>
2024-09-01 06:49:08 +0900

    Design : 판매글 작성 페이지 디자인(모바일 반응형) 구현

--- frontend/src/pages/PostCreatePage.js
+++ frontend/src/pages/PostCreatePage.js
@@ -1,75 +1,96 @@
 import React from 'react';
 import '../styles/pages/PostCreatePage.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
+import FormGroup from '../components/FormGroup';
+import RadioGroup from '../components/RadioGroup';
+import UploadButton from '../components/UploadButton';
 
 // 판매글 작성 페이지
 export default function PostCreatePage() {
   return (
     <section>
-      <div className="inner">
-        <h2>판매글 작성</h2>
-        <form>
-          {/* 상품명 */}
-          <div className="product-info">
-            <h3>상품명</h3>
-            <span>
-              <input type="text" />
-            </span>
-          </div>
-          {/* 카테고리 */}
-          <div className="product-info">
-            <h3>카테고리</h3>
-            <label htmlFor="kpop">
-              K-POP
-              <input type="radio" id="kpop" />
-            </label>
-            <label htmlFor="kpop">
-              애니메이션
-              <input type="radio" id="kpop" />
-            </label>
-            <label htmlFor="kpop">
-              영화/드라마
-              <input type="radio" id="kpop" />
-            </label>
-            <label htmlFor="kpop">
-              게임
-              <input type="radio" id="kpop" />
-            </label>
-            <label htmlFor="kpop">
-              스포츠
-              <input type="radio" id="kpop" />
-            </label>
-            <label htmlFor="kpop">
-              기타
-              <input type="radio" id="kpop" />
-            </label>
-          </div>
-          {/* 상품유형 */}
-          <div className="product-info">
-            <h3>상품유형</h3>
-            <input type="text" />
-          </div>
-          {/* 상품상태 */}
-          <div className="product-info">
-            <h3>상품상태</h3>
-            <input type="text" />
-          </div>
-          {/* 상품가격 */}
-          <div className="product-info">
-            <h3>상품가격</h3>
-            <input type="text" />
-          </div>
-          {/* 상품정보 */}
-          <div className="product-info">
-            <h3>상품정보</h3>
-            <input type="text" />
-          </div>
-          <div>
-            <input type="file" />
-          </div>
+      <h2>판매글 작성</h2>
+      <form id="form">
+        <FormGroup label="상품명">
+          <input
+            type="text"
+            name="product-name"
+            required
+            placeholder="상품명을 작성해주세요."
+          />
+        </FormGroup>
+
+        <FormGroup label="카테고리">
+          <RadioGroup
+            name="category"
+            options={[
+              { value: 'K-POP', label: 'K-POP' },
+              { value: '애니메이션', label: '애니메이션' },
+              { value: '영화/드라마', label: '영화/드라마' },
+              { value: '게임', label: '게임' },
+              { value: '기타', label: '기타' },
+            ]}
+            required
+          />
+        </FormGroup>
+
+        <FormGroup label="상품유형">
+          <RadioGroup
+            name="product-type"
+            options={[
+              { value: '공식', label: '공식' },
+              { value: '비공식', label: '비공식' },
+            ]}
+            required
+          />
+        </FormGroup>
+
+        <FormGroup label="상품상태">
+          <RadioGroup
+            name="product-status"
+            options={[
+              { value: '새상품', label: '새상품' },
+              { value: '중고', label: '중고' },
+            ]}
+            required
+          />
+        </FormGroup>
+
+        <FormGroup label="상품가격">
+          <input
+            type="number"
+            id="price"
+            name="price"
+            min="0"
+            step="0.01"
+            placeholder="가격을 입력하세요"
+          />
+          <span className="won">원</span>
+        </FormGroup>
+
+        <div className="form-group product-info">
+          <h3>상품정보</h3>
+          <textarea
+            name="description"
+            required
+            placeholder=" 작성 예시)
+            *상품명 : 
+            *구매시기 : 
+            *사용기간 : 
+            *하자여부 : 
+            *상세정보를 입력해주세요.
+            "
+          ></textarea>
+        </div>
+
+        <UploadButton />
+
+        <div className="btn-group">
           <button>취소</button>
-          <button>등록</button>
-        </form>
-      </div>
+          <button type="submit">등록</button>
+        </div>
+      </form>
     </section>
   );
 }
