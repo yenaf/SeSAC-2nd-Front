3ea7d512db1595e3b2d2f829df8c3cbee4ee9607
yenaf <spffhd0835@gmail.com>
2024-09-02 18:33:15 +0900

    Feat:판매게시글작성 페이지 상태를 formdata상태로 만듬

--- frontend/src/components/RadioGroup.js
+++ frontend/src/components/RadioGroup.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
-export default function RadioGroup({ name, options, required }) {
+export default function RadioGroup({
+  name,
+  options,
+  required,
+  register = () => {},
+}) {
   return (
     <div className="radio-group">
       {options.map((option) => (
-        <label className="category-btn" key={option.value}>
+        <label className={`category-btn`} key={option.value}>
           <input
             type="radio"
             name={name}
             value={option.value}
             required={required}
+            {...register(name)}
           />
           {option.label}
         </label>
