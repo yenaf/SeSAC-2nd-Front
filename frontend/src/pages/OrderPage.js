2983c9dc6f4dc882d60ec48b8ea86508d539a7ed
DAIN302 <dbgnfk12@gmail.com>
2024-09-02 16:03:27 +0900

    design : 장바구니 페이지 미디어쿼리

--- frontend/src/pages/OrderPage.js
+++ frontend/src/pages/OrderPage.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
+// 주문 번호가 랜덤으로 생성된다(숫자+문자 10자리)
+// 현재년도(숫자4자리) + 카테고리문자(문자1자리) + 랜덤숫자 5자리
+
 // 결제 페이지
 export default function OrderPage() {
-  return <div>OrderPage</div>;
+  return (
+    <div>
+      <h2>주문서</h2>
+    </div>
+  );
 }
