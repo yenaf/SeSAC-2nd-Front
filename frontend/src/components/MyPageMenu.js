35e4f69b00add2caa2cff272254c67c0316261e6
errorose <yoonyeseul2000@gmail.com>
2024-09-04 19:02:15 +0900

    feat: login기능

--- frontend/src/components/MyPageMenu.js
+++ frontend/src/components/MyPageMenu.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 
 export default function MyPageMenu() {
   return (
-    <nav className="mypage-list-container">
+    <nav className="mypage-list-container display-big">
       <ul>
-        <li>전체보기</li>
+        <li>
+          <Link to="/mypage">전체보기</Link>
+        </li>
         <li>구매내역</li>
         <li>판매내역</li>
         <li>판매글목록</li>
@@ -16,9 +18,9 @@ export default function MyPageMenu() {
           <ul className="mypage-edit">
             <li>- 개인정보수정</li>
             <li>- 배송지관리</li>
-            <Link to="/sellers">
-              <li>- 판매자정보등록</li>
-            </Link>
+            <li>
+              <Link to="/sellers">판매자정보등록</Link>
+            </li>
             <li>- 판매자정보수정</li>
             <li>- 회원탈퇴</li>
           </ul>
