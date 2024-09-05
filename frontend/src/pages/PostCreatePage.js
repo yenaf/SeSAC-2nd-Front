0159dac11d4fec8bdb95869dfff79ab3968ae237
yena <spffhd0835@gmail.com>
2024-09-06 03:20:57 +0900

    Feat:판매 게시글 상세페이지 api연결

--- frontend/src/pages/PostCreatePage.js
+++ frontend/src/pages/PostCreatePage.js
@@ -5,7 +5,6 @@ import RadioGroup from '../components/RadioGroup';
 import UploadButton from '../components/UploadButton';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import { insertPost } from '../api/post';
 
 // 판매글 작성 페이지
@@ -37,7 +36,7 @@ export default function PostCreatePage() {
         const res = await insertPost(postData);
         console.log(res.data.newPost);
         const { postId } = res.data.newPost;
-        navigate(`/posts/${postId}`, { state: { formData: res.data } });
+        navigate(`/posts/page/${postId}`, { state: { formData: res.data } });
       }
     } catch (error) {
       console.log(error);
@@ -54,113 +53,116 @@ export default function PostCreatePage() {
   };
 
   return (
-    <section className="post-write">
-      <h2 className="title">판매글 작성</h2>
-      <form
-        // action="/posts/create"
-        method="POST"
-        encType="multipart/form-data"
-        id="form"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <FormGroup label="상품명">
-          <input
-            type="text"
-            name="product-name"
-            placeholder="상품명을 작성해주세요."
-            required
-            {...register('postTitle', {
-              maxLength: { value: 60 },
-            })}
-          />
-        </FormGroup>
+    <>
+      <section className="post-write">
+        <h2 className="title">판매글 작성</h2>
+        <form
+          // action="/posts/create"
+          method="POST"
+          encType="multipart/form-data"
+          id="form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <FormGroup label="상품명">
+            <input
+              type="text"
+              name="product-name"
+              placeholder="상품명을 작성해주세요."
+              required
+              {...register('postTitle', {
+                maxLength: { value: 60 },
+              })}
+            />
+          </FormGroup>
 
-        <FormGroup label="카테고리">
-          <RadioGroup
-            name="categoryId"
-            options={[
-              { value: 1, label: 'K-POP' },
-              { value: 2, label: '영화/드라마' },
-              { value: 3, label: '애니메이션' },
-              { value: 4, label: '게임' },
-              { value: 5, label: '스포츠' },
-              { value: 6, label: '기타' },
-            ]}
-            required
-            register={register}
-          />
-        </FormGroup>
+          <FormGroup label="카테고리">
+            <RadioGroup
+              name="categoryId"
+              options={[
+                { value: 1, label: 'K-POP' },
+                { value: 2, label: '영화/드라마' },
+                { value: 3, label: '애니메이션' },
+                { value: 4, label: '게임' },
+                { value: 5, label: '스포츠' },
+                { value: 6, label: '기타' },
+              ]}
+              required
+              register={register}
+            />
+          </FormGroup>
 
-        <FormGroup label="상품유형">
-          <RadioGroup
-            name="productType"
-            options={[
-              { value: '공식', label: '공식' },
-              { value: '비공식', label: '비공식' },
-            ]}
-            required
-            register={register}
-          />
-        </FormGroup>
+          <FormGroup label="상품유형">
+            <RadioGroup
+              name="productType"
+              options={[
+                { value: '공식', label: '공식' },
+                { value: '비공식', label: '비공식' },
+              ]}
+              required
+              register={register}
+            />
+          </FormGroup>
 
-        <FormGroup label="상품상태">
-          <RadioGroup
-            name="productStatus"
-            options={[
-              { value: '새상품', label: '새상품' },
-              { value: '중고', label: '중고' },
-            ]}
-            required
-            register={register}
-          />
-        </FormGroup>
+          <FormGroup label="상품상태">
+            <RadioGroup
+              name="productStatus"
+              options={[
+                { value: '새상품', label: '새상품' },
+                { value: '중고', label: '중고' },
+              ]}
+              required
+              register={register}
+            />
+          </FormGroup>
 
-        <FormGroup label="상품가격">
-          <input
-            type="number"
-            id="price"
-            name="price"
-            min="0"
-            step="0.01"
-            placeholder="가격을 입력하세요"
-            required
-            {...register('productPrice', {
-              validate: {
-                positive: (value) => value > 0 || '가격은 0보다 커야 합니다.',
-              },
-            })}
-          />
-          <span className="won">원</span>
-        </FormGroup>
+          <FormGroup label="상품가격">
+            <input
+              type="number"
+              id="price"
+              name="price"
+              min="0"
+              max="100000000"
+              step="0.01"
+              placeholder="가격을 입력하세요"
+              required
+              {...register('productPrice', {
+                validate: {
+                  positive: (value) => value > 0 || '가격은 0보다 커야 합니다.',
+                },
+              })}
+            />
+            <span className="won">원</span>
+          </FormGroup>
 
-        <div className="form-group product-info">
-          <h3>상품정보</h3>
-          <textarea
-            name="description"
-            placeholder=" 작성 예시)
+          <div className="form-group product-info">
+            <h3>상품정보</h3>
+            <textarea
+              name="description"
+              placeholder=" 작성 예시)
             *상품명 : 
             *구매시기 : 
             *사용기간 : 
             *하자여부 : 
             *상세정보를 입력해주세요.
             "
-            required
-            {...register('postContent', {
-              maxLength: { value: 600 },
-            })}
-          ></textarea>
-          <span>{charCount} / 600</span>
-        </div>
+              required
+              {...register('postContent', {
+                maxLength: { value: 600 },
+              })}
+            ></textarea>
+            <span>{charCount} / 600</span>
+          </div>
 
-        <UploadButton register={register} />
+          <UploadButton register={register} />
 
-        <div className="btn-group">
-          <button type="button" onClick={onCancel}>
-            취소
-          </button>
-          <button type="submit">등록</button>
-        </div>
-      </form>
-    </section>
+          <div className="btn-group">
+            <button type="button" onClick={onCancel}>
+              취소
+            </button>
+            <button type="submit">등록</button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 }
--- frontend/src/pages/PostDetailPage.js
+++ frontend/src/pages/PostDetailPage.js
@@ -8,12 +8,33 @@ import {
   faVolumeOff,
 } from '@fortawesome/free-solid-svg-icons';
 import priceToString from '../utils/priceMethods';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Comment from '../components/Comment';
+import axios from 'axios';
+import { formatDate } from '../components/common/formatDate';
+
+// 상세게시글에 들어오려면 판매글작성후 또는 게시글을 눌렀을때
 
 export default function PostDetailPage() {
   const navigate = useNavigate();
-  const [isDibbed, setIsDibbed] = useState(false); // 상태 추가
+  const [isDibbed, setIsDibbed] = useState(false);
+  const [postData, setPostData] = useState(null);
+
+  useEffect(() => {
+    // url에서 postId 가져옴
+    const currentUrl = window.location.href;
+    const id = currentUrl.split('/').pop();
+    const res = axios.get(`http://localhost:8080/posts/${id}`);
+
+    res
+      .then((res) => {
+        setPostData(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error('API 호출 중 오류 발생:', error);
+      });
+  }, []);
 
   const handleBackPage = () => {
     // 만약에 이전페이지가 게시글작성이라면 메인페이지로가야한다
@@ -21,104 +42,139 @@ export default function PostDetailPage() {
   };
 
   const handleChangeDibs = () => {
-    setIsDibbed(!isDibbed); // 상태 토글
+    setIsDibbed(!isDibbed); // 찜 상태 토글
   };
+
+  const {
+    postTitle,
+    Product_Images: productImg,
+    categoryId,
+    postContent,
+    productPrice,
+    productStatus,
+    productType,
+    createdAt: time,
+    Seller: seller,
+  } = postData;
+
+  const createdAt = formatDate(time);
+
+  const getCategoryLabel = (id) => {
+    switch (Number(id)) {
+      case 1:
+        return 'K-POP';
+      case 2:
+        return '영화/드라마';
+      case 3:
+        return '애니메이션';
+      case 4:
+        return '게임';
+      case 5:
+        return '스포츠';
+      case 6:
+        return '기타';
+      default:
+        return '알 수 없는 카테고리';
+    }
+  };
+
   return (
     <>
-      <section className="post-desc">
-        {/* 상품이미지 */}
-        <div className="post-top">
-          <div className="product-img">
-            <SwiperMagnify />
-          </div>
-          {/* 우측 나열될 정보 */}
-          <div className="product-info">
-            <strong>애니메이션</strong>
-            <h2 title="">
-              쿠로미 인형 상태좋아요 진짜
-              올롤로로로ddddddddddddddddddddddddddddddddddd
-            </h2>
-            <h3>{priceToString('10000')} 원</h3>
-            {/* <time>작성시간</time> */}
-            <div className="info-box">
-              <div>
-                <span>배송사</span>
-                <span>우체국</span>
+      {!postData ? (
+        <div>로딩 중...</div>
+      ) : (
+        <div>
+          <section className="post-desc">
+            {/* 상품이미지 */}
+            <div className="post-top">
+              <div className="product-img">
+                <SwiperMagnify productImg={productImg} />
               </div>
-              <div>
-                <span>배송비</span>
-                <span>{priceToString('3000')}</span>
-              </div>
-              <div>
-                <span>상품유형</span>
-                <span>공식</span>
-              </div>
-              <div>
-                <span>상품상태</span>
-                <span>새상품</span>
+              {/* 우측 나열될 정보 */}
+              <div className="product-info">
+                <time>{createdAt}</time>
+                <strong>{getCategoryLabel(`${categoryId}`)}</strong>
+                <h2 title="">{postTitle}</h2>
+                <h3>{priceToString(`${productPrice}`)} 원</h3>
+                <div className="info-box">
+                  <div>
+                    <span>배송사</span>
+                    <span>우체국</span>
+                  </div>
+                  <div>
+                    <span>배송비</span>
+                    <span>{priceToString('3000')}</span>
+                  </div>
+                  <div>
+                    <span>상품유형</span>
+                    <span>{productType}</span>
+                  </div>
+                  <div>
+                    <span>상품상태</span>
+                    <span>{productStatus}</span>
+                  </div>
+                </div>
+                <div className="btn-group">
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    className="dibs"
+                    onClick={handleChangeDibs}
+                    style={{ color: isDibbed ? '#ba357e' : '#c9c9c9' }}
+                  />
+                  <span className="link-container">
+                    <Link to={'/cart/:userId'} className="btn shopping">
+                      장바구니
+                    </Link>
+                    <Link to={'/order'} className="btn buy">
+                      구매하기
+                    </Link>
+                  </span>
+                </div>
               </div>
             </div>
-
-            <div className="btn-group">
+            {/* 하단 상품설명 */}
+            <div className="post-botton">
               <FontAwesomeIcon
-                icon={faHeart}
-                className="dibs"
-                onClick={handleChangeDibs}
-                style={{ color: isDibbed ? '#ba357e' : '#c9c9c9' }}
+                icon={faVolumeOff}
+                className="report-icon"
+                title="신고하기"
               />
-              <span className="link-container">
-                <Link to={'/cart/:userId'} className="btn shopping">
-                  장바구니
-                </Link>
-                <Link to={'/order'} className="btn buy">
-                  구매하기
-                </Link>
-              </span>
+              {/* 판매자 배너 */}
+              <div className="seller-info">
+                <div className="seller-profile">
+                  <img src="/img/cat.png" className="seller-img" />
+                </div>
+                <h3>{seller.sellerName}</h3>
+              </div>
+              <div className="product-content">
+                <p>{postContent}</p>
+              </div>
             </div>
-          </div>
-        </div>
-
-        {/* 하단 상품설명 */}
-        <div className="post-botton">
-          <FontAwesomeIcon
-            icon={faVolumeOff}
-            className="report-icon"
-            title="신고하기"
-          />
-          {/* 판매자 배너 */}
-          <div className="seller-info">
-            <div className="seller-profile">
-              <img src="/img/cat.png" className="seller-img" />
+            <div className="btn-bottom-group">
+              <div className="histoty-btn">
+                <button className="btn back" onClick={handleBackPage}>
+                  <FontAwesomeIcon icon={faAngleLeft} className="back-icon" />
+                  돌아가기
+                </button>
+                <button className="btn list">
+                  <Link
+                    to={'/posts/list/:page/:limit/:categoryId'}
+                    className="list-link"
+                  >
+                    목록
+                  </Link>
+                </button>
+              </div>
+              <div className="ud-btn">
+                <button className="btn correction">수정</button>
+                <button className="btn delete">삭제</button>
+              </div>
             </div>
-            <h3>판매자명</h3>
-          </div>
-          <div className="product-content">
-            <p>설명글postContent 어쩌고저쩌고</p>
-          </div>
+          </section>
+          {/* 댓글 */}
+          <Comment />
         </div>
-        <div className="btn-bottom-group">
-          <div className="histoty-btn">
-            <button className="btn back" onClick={handleBackPage}>
-              <FontAwesomeIcon icon={faAngleLeft} className="back-icon" />
-              돌아가기
-            </button>
-            <button className="btn list">
-              <Link
-                to={'/posts/list/:page/:limit/:categoryId'}
-                className="list-link"
-              >
-                목록
-              </Link>
-            </button>
-          </div>
-          <div className="ud-btn">
-            <button className="btn correction">수정</button>
-            <button className="btn delete">삭제</button>
-          </div>
-        </div>
-      </section>
-      {/* 댓글 */}
-      <Comment />
+      )}
     </>
   );
 }
