dbf12f30e0f9f8c8e23c4904441a64afb5c16d56
DAIN302 <dbgnfk12@gmail.com>
2024-09-09 03:12:42 +0900

    feat: 배송지 조회

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import priceToString from '../utils/priceMethods';
 import elapsedTime from '../utils/elapsedTime';
+import handleScrollToTop from '../utils/handleScrollToTop';
 import { Link } from 'react-router-dom';
 
 export default function ItemList({ item }) {
   const imgUrl = 'https://lieblings-bucket.s3.ap-northeast-2.amazonaws.com/';
   return (
     <li className="list-item">
-      <Link to={`/posts/page/${item.postId}`}>
+      <Link to={`/posts/page/${item.postId}`} onClick={handleScrollToTop}>
         <figure className="item-img">
           <img
             src={`${imgUrl}${item.Product_Images[0].imgName}`}
