4c9d4e8306cfdb14187c3a3a91f4a95c731ca4ce
DAIN302 <dbgnfk12@gmail.com>
2024-09-16 00:51:43 +0900

    fix : 장바구니 페이지 fix

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -7,17 +7,27 @@ import { loadCart } from '../store/cartSliceTemp';
 
 // 장바구니 페이지
 export default function CartPage() {
-  const cart = useSelector((state) => state.cart.cartData);
+  const { cartData, loading } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadCart());
-  }, []);
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <div className="loading-inner"></div>
+      </div>
+    );
+  }
 
   return (
     <>
       <h2 className="cart-title">장바구니</h2>
-      <div className="cart">{cart.length > 0 ? <Cart /> : <CartEmpty />}</div>
+      <div className="cart">
+        {cartData.length > 0 ? <Cart /> : <CartEmpty />}
+      </div>
     </>
   );
 }
