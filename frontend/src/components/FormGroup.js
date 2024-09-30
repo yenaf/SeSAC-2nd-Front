d566299c34e3020eb39a4209e10ef8e0ec9d51f3
yenaf <spffhd0835@gmail.com>
2024-09-30 15:45:07 +0900

    Refactor : FormGroup컴포넌트에 React.memo 사용

--- frontend/src/components/FormGroup.js
+++ frontend/src/components/FormGroup.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 // 폼 입력 컴포넌트
-export default function FormGroup({ label, children, style }) {
+const FormGroup = React.memo(({ label, children, style }) => {
   return (
-    <div className="form-group " style={style}>
+    <div className="form-group" style={style}>
       <h3 className="post-title">{label}</h3>
       {children}
     </div>
   );
-}
+});
+
+// displayName 설정
+FormGroup.displayName = 'FormGroup';
+
+export default FormGroup;
